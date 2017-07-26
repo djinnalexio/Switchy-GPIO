e62dc6b33db28165f5a05d4030b5107b5fffaa4f
Andre <pakueamb@gmail.com>
2017-07-26 02:19:08 +0200

    In progress (15%)

--- Switchy.py
+++ Switchy.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import RPi.GPIO as Pin
 import time
 Pin.setmode(Pin.BCM)
@@ -16,29 +19,82 @@ Pin.setup(white2, Pin.OUT)
 yellow=16
 Pin.setup(yellow, Pin.OUT)
 
+color = (blue, green, red, white, white2, yellow)
+
 def turnON(color):
+	"Turns on lights"
 	Pin.output(color,Pin.HIGH)
+	print "The %s light(s) is now on." % str(color)
+	time.sleep(1.5)
 
 def turnOFF(color):
+	"Turns off lights"
 	Pin.output(color,Pin.LOW)
+	print "The %s light(s) is now off." % str(color)
+	time.sleep(1.5)
 
 def blink(color, duration):
+	"Makes lights blink for <duration> seconds"
+	step = 1.0 / 6.0
+	print "The %s light(s) will be blinking for %i seconds." % (str(color), duration)
+	print "		Starting in",
+	for i in range(0,2):
+		print 3 - i
 	count = range(0,duration)
 	for i in count:
 		turnON(color)
-		time.sleep(0.2)
+		time.sleep(step)
 		turnOFF(color)
-		time.sleep(0.2)
+		time.sleep(step)
 		turnON(color)
-		time.sleep(0.2)
+		time.sleep(step)
 		turnOFF(color)
-		time.sleep(0.2)
+		time.sleep(step)
 		turnON(color)
-		time.sleep(0.2)
-	print "end"
+		time.sleep(step)
+		turnOFF(color)
+		time.sleep(step)
+	print "Well, 'looks like that's it! anything else?"
 
-def passing_shadow(duration):
+#def passing_shadow(duration):
 
-def alt_blink(duration):
+#def alt_blink(duration):
 
-def countsec(duration):
+#def countdown(duration):
+
+
+
+#Start of the app
+
+
+print ("""
+☻ ☼ ☽ ☾ ♠ ♡ ♢ ♣ ♤ ♥ ♦ ♧ ♨ ♩ ✙ ✈ ✉ ✌ ✁ ✎ ✐ ❀ ✰ ❁ ☻ ☼ ☽ ☾ ♠ ♡ ♢ ♣ ♤ ♥ ♦ ♧ ♨ ♩ 
+
+	Hello friend. Nice to meet you! I'm Switchy!^‿^""")
+
+raw_input()
+
+print ("""
+	I bet you're here because you wanna know how to
+	  turn on the lights connected to the Pi!^‿^""")
+
+raw_input()
+
+print ("""
+	If I'm right, then don't worry 'cause you're at
+	              the right place!^‿^""")
+
+raw_input()
+
+print("""
+	^‿^You see, I'm the application that allows you
+	          to control those lights.""")
+
+raw_input()
+
+username = raw_input("""
+		But before we get to business,
+		   may I have your name? •‿•
+			◊""")
+
+
