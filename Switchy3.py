6a613f4e03944ad75d800af22f816ec972cc9edb
pakueamb <pakueamb@gmail.com>
2018-01-09 17:58:03 -0500

    setting up classes

--- Switchy3.py
+++ Switchy3.py
@@ -11,32 +11,32 @@ GPIO.setwarnings(True)
 class lights:
 
     def __init__(self,color_name,color_pin):
-	    self.name = color_name
-		self.pin = color_pin
-	
-	GPIO.setup(self.pin, GPIO.OUT)
-	
-	def check_state(self):
-	    if GPIO.input(self.pin):
-		    print ("The " + self.color_name + "light is on.")
-		elif GPIO.input(self.pin) == 0:
-		    print ("The " + self.color_name + "light is off.")
-	
-	def toggle_switch(self):
-	    if GPIO.input(self.pin):
-		    GPIO.output(self.pin,0)
-			print ("The " + self.color_name + " was turned off.")
-		elif GPIO.input(self.pin) == 0:
-		    GPIO.output(self.pin,1)
-			print ("The " + self.color_name + " was turned on.")
+        self.name = color_name
+        self.pin = color_pin
 
-L1 = lights(L1, "white", 21)
-L2 = lights(L2, "yellow", 16)
-L3 = lights(L3, "red", 12)
-L4 = lights(L4, "blue", 25)
-L5 = lights(L5, "green", 24)
-L6 = lights(L6, "white", 13)
+    GPIO.setup(self.pin, GPIO.OUT)
+
+    def check_state(self):
+        if GPIO.input(self.pin):
+            print ("The " + self.color_name + "light is on.")
+        elif GPIO.input(self.pin) == 0:
+            print ("The " + self.color_name + "light is off.")
+
+    def toggle_switch(self):
+        if GPIO.input(self.pin):
+            GPIO.output(self.pin,0)
+            print ("The " + self.color_name + " was turned off.")
+        elif GPIO.input(self.pin) == 0:
+            GPIO.output(self.pin,1)
+            print ("The " + self.color_name + " was turned on.")
+
+L1 = lights("white", 21)
+L2 = lights("yellow", 16)
+L3 = lights("red", 12)
+L4 = lights("blue", 25)
+L5 = lights("green", 24)
+L6 = lights("white", 13)
 
 for i in [L1,L2,L3,L4,L5,L6]:
     time.sleep(1)
-	i.check_state()
+    lights.check_state(i)
