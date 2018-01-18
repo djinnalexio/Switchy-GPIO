fbfeb46838e7a6afb078aa09b8a0298d57b4ecd5
pakueamb <pakueamb@gmail.com>
2018-01-18 14:40:29 -0500

    Used classes to order objects and shorten code

--- Switchy.py
+++ Switchy.py
@@ -1,325 +1,233 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import random
+
+__author__ = "Andre Akue"
+
+import RPi.GPIO as GPIO
 import time
-import RPi.GPIO as pin
-pin.setmode(pin.BCM)
-pin.setwarnings(True)
+from os import system
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(True)
 
-white=21 #40 commented out numbers here represent the physical number of the pins
-yellow=16 #36
-red=12 #32
-blue=25 #22
-green=24 #18
-white2=13 #33
-white= (white,white2) 
-colors = (blue, green, red, white2, white, yellow)
-GPIO.setup(colors,GPIO.OUT) #setups up all pins at once as outputs for the lights
+class lights:
 
-color_list = ['all', 'left', 'yellow', 'red', 'blue', 'green', 'right', 'white']
-led_list = [left, yellow, red, blue, green, right]
+	def __init__(self,color_name,color_pin):
+		self.name = color_name
+		self.pin = color_pin
+		GPIO.setup(self.pin, GPIO.OUT, initial = 1)
 
-def switch():
-	print ("Do you want to turn on or turn off a light?")
-	while True:
-		O = raw_input("""
-1 -> on	2 -> off	3 -> Check status
+	def check_state(self):
+		if GPIO.input(self.pin):
+			print ("\tThe " + self.name + " light is on.")
+		elif GPIO.input(self.pin) == 0:
+			print ("\tThe " + self.name + " light is off.")
 
-0 -> back to main menu
-		""")
-		if O == '1' or O.lower() == 'on':
-			ask_color('turn on',turnON)
-		elif O == '2' or O.lower() == 'off':
-			ask_color('turn off',turnOFF)
-		elif O == '3':
-			ask_color('check',check)
-		elif O == '0':
-			break
+	def toggle_switch(self,color):
+		if color == self.name:
+			if GPIO.input(self.pin):
+				GPIO.output(self.pin,0)
+				print ("\tThe " + self.name + " light was turned off.")
+			elif GPIO.input(self.pin) == 0:
+				GPIO.output(self.pin,1)
+				print ("\tThe " + self.name + " light was turned on.")
 		else:
-			print ("Please try again")
+			pass
 
-def ask_color(a,b):
-	while True:
-		ask_col = "Which light do you want to %s?\n\t>" % a
-		color = raw_input(ask_col).lower()
-		time.sleep(0.5)
+	def blinking(self,color):
+		if color == self.name:
+			system('clear')
+			stp = time.time() ; etp = time.time() + duration # stp/etp = starting / ending time pattern
+			while time.time() < etp:
+				print ("\n\n\n\n\n\n\n\tThe " + self.name + " light is blinking.")
+				print "\n\n\t\t%i/%i seconds" % (int(time.time() - stp),duration)
+				GPIO.output(self.pin,0)
+				time.sleep(0.5/speed)
+				GPIO.output(self.pin,1)
+				time.sleep(0.5/speed)
+				system('clear')
+		else:
+			pass	
+
+class settings:
+	def display_settings(self):
+		print ("\n\nSpeed of displays: lv{0}\n\nDuration of displays: {1} seconds\n\n\n\n".format(speed,duration))
+		raw_input()
 		
-		if color in color_list:
-			b(eval(color),color)
-			return
-		
-		else:
-			print 'sorry'
-			return
+	def set_speed(self):
+		try:
+			speed = int(raw_input("choose a speed between 1 and 10\n\t>"))
+			if speed <= 10 and speed >=1:
+				print "\nDuration changed"
+				time.sleep(1)
+				return speed
+			else:
+				print("Sorry. This value is invalid. speed set back to default")
+				time.sleep(2)
+				return 3
+		except ValueError:
+			print("Sorry. You entered in invalid input. speed set back to default")
+			time.sleep(2)
+			return 3
 
-def check(color,color_name):
-	try:
-		if pin.input(color) == True:#only works with one integer
-			print '\n%s is on' % color_name
-		else:
-			print '\n%s is off' % color_name
-	except:
-		print '\ncan\'t check this color'
+	def set_duration(self):
+		try:
+			duration = int(raw_input("\n\nchoose a duration for the displays\n\t>"))
+			print "\nDuration changed"
+			time.sleep(1)
+			return duration
+		except ValueError:
+			print("Sorry. You entered in invalid input. duration set back to default")
+			time.sleep(2)
+			return 20
 
-def turnON(color,color_name):
-	"Turns on lights"
-	pin.output(color,pin.HIGH)#Instead of 'pin.HIGH', True can also work
-	if color_name == 'all':
-		print "\nAll the lights are now on. •‿•\n"
-	else:
-		print "\nThe %s light is now on. •‿•\n" % color_name
-	time.sleep(0.5)
+class light_shows:
+	def moving_light(self):
+		for i in [L1,L2,L3,L4,L5,L6]:
+			GPIO.output(i.pin,0)
+		system('clear')
+		stp = time.time() ; etp = time.time() + duration # stp/etp = starting / ending time pattern
+		while time.time() < etp:
+			print ("\n\n\n\n\n\n\n\tMOVING LIGHT")
+			print "\n\n\t\t%i/%i seconds" % (int(time.time() - stp),duration)
+			for i in [L1,L2,L3,L4,L5,L6]:
+				GPIO.output(i.pin,True)
+				time.sleep(0.5/speed)
+				GPIO.output(i.pin,False)
+				system('clear')
 
-def turnOFF(color,color_name):
-	"Turns off lights"
-	pin.output(color,pin.LOW)#Instead of 'pin.LOW', False can also work
-	if color_name == 'all':
-		print "\nAll the lights are now off. •‿•\n"
-	else:
-		print "\nThe %s light is now off. •‿•\n" % color_name
-	time.sleep(0.5)
+	def moving_shadow(self):
+		for i in [L1,L2,L3,L4,L5,L6]:
+			GPIO.output(i.pin,1)
+		system('clear')
+		stp = time.time() ; etp = time.time() + duration # stp/etp = starting / ending time pattern
+		while time.time() < etp:
+			print ("\n\n\n\n\n\n\n\tMOVING LIGHT")
+			print "\n\n\t\t%i/%i seconds" % (int(time.time() - stp),duration)
+			for i in [L1,L2,L3,L4,L5,L6]:
+				GPIO.output(i.pin,False)
+				time.sleep(0.5/speed)
+				GPIO.output(i.pin,True)
+				system('clear')
 
-def blink(color,color_name):
-	"Makes lights blink for <duration> seconds"
+	def police_car(self):
+		for i in [L1,L2,L3,L4,L5,L6]:
+			GPIO.output(i.pin,0)
+		system('clear')
+		GPIO.output([L1.pin,L6.pin],1)
+		stp = time.time() ; etp = time.time() + duration # stp/etp = starting / ending time pattern
+		while time.time() < etp:
+			print ("\n\n\n\n\n\n\n\tMOVING LIGHT")
+			print "\n\n\t\t%i/%i seconds" % (int(time.time() - stp),duration)
+			for i in [L3,L4]:
+				GPIO.output(i.pin,True)
+				time.sleep(0.5/speed)
+				GPIO.output(i.pin,False)
+				system('clear')
 
-	if color_name == 'all':
-		ask_d = "\nFor how long do you want the lights to blink? (in seconds)\n\t\t>"
-	else:
-		ask_d = "\nFor how long do you want the %s light to blink? (in seconds)\n\t\t>" % color_name
-	ask_duration = raw_input(ask_d)
-	if ask_duration.isdigit():
-		duration = int(ask_duration)
-	else:
-		print ("sorry")
-		time.sleep(1)
-		return
-	
-	ask_blink_speed = raw_input("\nChoose a blinking speed •‿•\n\t\t>")
-	if ask_blink_speed.isdigit() :
-		inter = 0.5 / int(ask_blink_speed)
-	else:
-		print ("sorry")
-		time.sleep(1)
-		return
-	
-	stp = time.time() ; etp = time.time() + duration# stp/etp = starting / ending time pattern
-	while time.time() < etp: 
-		print (int(time.time() - stp))# elased time = current time - time start blink
-		pin.output(color,True)
-		time.sleep(inter)
-		pin.output(color,False)
-		time.sleep(inter)
-			
-	print "\ndone\n"
-	time.sleep(0.5)
+def ask_single_color(function):
+    while True:
+        ask_color = raw_input("\n\tWhich light?\n\t\t>")
+        color = ask_color.lower()
 
-def ask_time():
-	duration = raw_input("\nFor how long do you want the pattern to run for? (in seconds)\n\t>")
-	if duration.isdigit():
-		duration = int(duration)
-	else:
-		return
-	
-	print "\n\tThe light show will run for about %s seconds." % duration
-	time.sleep(1)
-	return duration
+        for i in [L1,L2,L3,L4,L5,L6]:
+           function(i,color)
 
-def ask_time_and_speed():
-	duration = raw_input("\nFor how long do you want the pattern to run for? (in seconds)\n\t>")
-	if duration.isdigit():
-		duration = int(duration)
-	else:
-		return
-	
-	speed = raw_input("\nChoose a speed for the lights•‿•\n\t>")
-	if speed.isdigit() :
-		inter = 0.5/int(speed)
-	else:
-		return
-	
-	print "\n\tThe light show will run for about %s seconds.\n\tSpeed: %s." % (duration, speed)
-	time.sleep(1)
-	return duration, inter
+        if color == 'none' or color == '':
+            print("see ya\n")
+            break
 
-def light_shows():
-	print "Here are light shows"
-	time.sleep(1)
-	while True:
-		print("""
-1:Moving Light	2:Moving Shadow	3:Police Car
-
-4:Back and Forth	5:Two by Two	6:Random
-
-0:Back to main menu
+        elif color not in [L1.name,L2.name,L3.name,L4.name,L5.name,L6.name]:
+            print("""
+✖﹏✖ Sorry but this color is not available. ✖﹏✖
 """)
 
-		l_s = raw_input("""			Type a number here ->""")
+L1 = lights("white", 21)
+L2 = lights("yellow", 16)
+L3 = lights("red", 12)
+L4 = lights("blue", 25)
+L5 = lights("green", 24)
+L6 = lights("snow", 13)
 
-		if l_s == '1':#Moving Light
-			try:
-				duration, inter = ask_time_and_speed()
-			except TypeError:
-				print 'not number'
-				break
-			moving_light(duration,inter)
-			time.sleep(0.5)
+main_menu = """Controlling single lights
 
-		elif l_s == '2':#Moving Shadow
-			try:
-				duration, inter = ask_time_and_speed()
-			except TypeError:
-				print 'not number'
-				break
-			moving_shadow(duration,inter)
-			time.sleep(0.5)
+1: Check light status	2: Switch	3: Make it blink
 
-		elif l_s == '3':#police car
-			try:
-				duration = ask_time()
-			except TypeError:
-				print 'not number'
-				break
-			police_car(duration)
-			time.sleep(0.5)
 
-		elif l_s == '4':#back an'forth
-			try:
-				duration, inter = ask_time_and_speed()
-			except TypeError:
-				print 'not number'
-				break
-			back_an_forth(duration,inter)
-			time.sleep(0.5)
+For all lights
 
-		elif l_s == '5':#two by two
-			try:
-				duration, inter = ask_time_and_speed()
-			except TypeError:
-				print 'not number'
-				break
-			two_by_two(duration,inter)
-			time.sleep(0.5)
+4: Lights on	5: Blackout
 
-		elif l_s == '6':#Random
-			try:
-				duration, inter = ask_time_and_speed()
-			except TypeError:
-				print 'not number'
-				break
-			rm(duration,inter)
-			time.sleep(0.5)
 
-		elif l_s == '0':#exit light shows
-			break
+Light displays
 
-		else:#not defined
-			time.sleep(1)
-			print("""not defined""")
-			time.sleep(1)
+6: Moving light	7: Moving shadow	8: Police Car
 
-def moving_light(duration,inter):
-	pin.output(all,False)
-	print ('code is running')
-	etp = time.time() + duration # etp = ending time pattern
-	while time.time() <= etp:		
-		for i in led_list:
-			pin.output(i,True)
-			time.sleep(inter)
-			pin.output(i,False)
 
-def moving_shadow(duration,inter):
-	pin.output(all,True)
-	print ('code is running')
-	etp = time.time() + duration # etp = ending time pattern
-	while time.time() <= etp:		
-		for i in led_list:
-			pin.output(i,False)
-			time.sleep(inter)
-			pin.output(i,True)
-	pin.output(all,False)
-	
-def police_car(duration):
-	led_list = [blue,red]
-	pin.output(all,False)
-	time.sleep(.75)
-	pin.output(white,True)
-	time.sleep(.5)
-	print ('code is running')
-	etp = time.time() + duration # etp = ending time pattern
-	while time.time() <= etp:		
-		for i in led_list:
-			pin.output(i,True)
-			time.sleep(.2)
-			pin.output(i,False)
-	pin.output(all,False)
+Settings
 
-def back_an_forth(duration,inter):
-	l1 = (red,blue)
-	l2 = (yellow,green)
-	light_list = [l1,l2]
-	pin.output(all,False)
-	time.sleep(.75)
-	pin.output(white,True)
-	time.sleep(.5)
-	print ('code is running')
-	etp = time.time() + duration # etp = ending time pattern
-	while time.time() <= etp:		
-		for i in light_list:
-			pin.output(i,True)
-			time.sleep(inter)
-			pin.output(i,False)
-	pin.output(all,False)
+10: Change Speed of displays	11: Change Duration
 
-def two_by_two(duration,inter):
-	l1 = (left,yellow)
-	l2 = (red,blue)
-	l3 = (green,right)
-	light_list = [l1,l2,l3]
-	pin.output(all,False)
-	print ('code is running')
-	etp = time.time() + duration # etp = ending time pattern
-	while time.time() <= etp:		
-		for i in light_list:
-			pin.output(i,True)
-			time.sleep(inter)
-			pin.output(i,False)
+12: Display current settings
 
-def rm(duration,inter):
-	pin.output(all,False)
-	print ('code is running')
-	etp = time.time() + duration # etp = ending time pattern
-	while time.time() <= etp:		
-		for i in led_list:
-			j = random.choice(led_list)
-			pin.output(j,True)
-			time.sleep(0.1)
-			pin.output(j,False)
-			time.sleep(inter)
+
+
+20: Exit Switchy
+
+	>"""
+
+speed = 3
+duration = 20
+
+sets = settings()
+show = light_shows()
+
+time.sleep(2)
 
 try:
 	while True:
-		print("""
-1 -> Light Switch		2 -> Blink
-
-3 -> Light Shows
-		""")
-		action = raw_input("""			 ->""")
-		if action == '1':
-			switch()
-			
-		elif action == '2':
-			ask_color('blink',blink)
+		system('clear')
+		a = raw_input(main_menu)
 		
-		elif action == '3':
-			light_shows()
-			
+		if a == "1":
+			print '\n'
+			for i in [L1,L2,L3,L4,L5,L6]:
+			   i.check_state()
+			raw_input()
+		elif a == "2":
+			ask_single_color(lights.toggle_switch)
+		elif a == "3":
+			ask_single_color(lights.blinking)
+		elif a == "4":#Lights on
+			for i in [L1,L2,L3,L4,L5,L6]:
+			   GPIO.output(i.pin,1)
+		elif a == "5":#Blackout
+			for i in [L1,L2,L3,L4,L5,L6]:
+			   GPIO.output(i.pin,0)
+		elif a == "6":
+			light_shows.moving_light(show)
+		elif a == "7":
+			light_shows.moving_shadow(show)
+		elif a == "8":
+			light_shows.police_car(show)
+		elif a == "10":
+			speed = settings.set_speed(sets)
+		elif a == "11":
+			duration = settings.set_duration(sets)
+		elif a == "12":
+			settings.display_settings(sets)
+		elif a == "20":#exit
+			print ("bye")
+			GPIO.cleanup()
+			exit()
+		elif a == "":
+			pass
 		else:
-			print 'not defined'
-			break	
+			time.sleep(0.5)
+			system('clear')
+			print ("\n\n\n\n\nSorry. Invalid input")
+			time.sleep(2)
 
-	print 'Safe exit. GPIO pins cleaned'
-	pin.cleanup()
-	
 except KeyboardInterrupt:
-	print "code interrupted. GPIO pins cleaned"
-	pin.cleanup()
+    system('clear')
+    print ("\n\n\texited via keyboard interrupt\n\n")
+    GPIO.cleanup()
