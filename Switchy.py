8a2f898e86e56e0837f693087924ecf1e199524a
pakueamb <pakueamb@gmail.com>
2018-01-23 21:37:38 -0500

    changed the pin for Snow light

--- Switchy.py
+++ Switchy.py
@@ -146,7 +146,7 @@ L2 = lights("yellow", 16)
 L3 = lights("red", 12)
 L4 = lights("blue", 25)
 L5 = lights("green", 24)
-L6 = lights("snow", 13)
+L6 = lights("snow", 23)
 
 main_menu = """Controlling single lights
 
